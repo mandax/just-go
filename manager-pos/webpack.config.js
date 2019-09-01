84fd9a34dfd00b6ae4d619e73a2dfa1d1bd9d80f
Mandax (Anderson F. Pinto) <mandax.contato@gmail.com>
2019-08-31 21:06:10 -0300

    adding react-icons and improving webpack config

--- manager-pos/webpack.config.js
+++ manager-pos/webpack.config.js
@@ -1,6 +1,11 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
+
+dotenv.config();
 
 module.exports = {
 	mode: "production",
@@ -28,6 +33,21 @@ module.exports = {
 			}
 		]
 	},
+
+	plugins: [
+		// passing JG_APP_ vars to the project
+		new webpack.DefinePlugin(Object.keys(process.env).reduce(
+			(acc, key) => {
+				acc[key] = process.env[key];
+				return acc;
+			}, 
+		{})),
+
+		new CopyPlugin([{
+			from: path.resolve(__dirname, 'static'),
+			to: path.resolve(__dirname, 'dist')
+		}])
+	],
 	
 	externals: {
 		"react": "React",
