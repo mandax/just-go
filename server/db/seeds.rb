8b62597fccd6c4c7e0a847b907d3714dcefc2d7f
Anderson F. Pinto (mandax) <mandax.contato@gmail.com>
2019-09-06 20:40:37 -0300

    update seed

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -10,12 +10,13 @@ end
 	
 	price = Faker::Number.decimal(l_digits: 2, r_digits: 2)
 	cost = price * (Faker::Number.within(range: 0...80).to_f / 100)
+	name = Faker::Food.dish
 
 	Item.create({
-		name: Faker::Food.dish,
+		name: name,
 		description: Faker::Food.description,
 		category: Category.select(:id).order('RANDOM()').first,
-		picture: 'https://loremflickr.com/1024/1024/food',
+		picture: "http://lorempixel.com/640/480/food",
 		max_discount: Faker::Number.within(range: 0..20),
 		price: Faker::Number.decimal(l_digits: 2, r_digits: 2),
 		cost: cost.to_i
