324cff59ccc126ee0671b8e514798aa92be9facd
Anderson F. Pinto (mandax) <mandax.contato@gmail.com>
2019-09-10 12:36:19 -0300

    some instructions

--- packages/ui/stories/card.default.stories.js
+++ packages/ui/stories/card.default.stories.js
@@ -14,14 +14,21 @@ storiesOf('Components|Card', module)
     let image = text('image URL', 'https://www.demilked.com/magazine/wp-content/uploads/2019/01/5c41e16e8c8f3-powder-dog-photography-animals-coverimage.jpg')
 
     return (
-      <div style={{ ...container(), ...horizontalCenter, maxWidth: '300px' }}>
-        <Card selected={selected}>
-          <CardImage src={image} />
-          <h3 style={titleSpacing(1)}>Hello</h3>
-          <div>
-            This is a cool fancy card.
-          </div>
-        </Card>
+      <div style={container()}>
+        <p>
+          The Card will fill the available width, to set a size you must set it on the parent width or use a Grid Component.
+        </p>
+        <br />
+
+        <div style={{ ...horizontalCenter, maxWidth: '300px' }}>
+          <Card selected={selected}>
+            <CardImage src={image} />
+            <h3 style={titleSpacing(1)}>Hello</h3>
+            <div>
+              This is a cool fancy card.
+            </div>
+          </Card>
+        </div>
       </div>
     )
   })
